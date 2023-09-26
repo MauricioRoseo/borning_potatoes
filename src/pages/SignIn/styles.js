c127866d8dfb68f34d30455d6ec11ce7222c6037
Mauricio <mauriciornn@gmail.com>
2023-09-26 11:24:16 -0300

    atualização-26/09

---
+++ src/pages/SignIn/styles.js
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+export const Background = styled.View`
+  flex: 1;
+  background-color: #f0f4ff;
+`;
+
+export const Container = styled.KeyboardAvoidingView``;
+
+export const Logo = styled.Image``;
+
+export const AreaInput = styled.View``;
+
+export const Input = styled.TextInput``;
